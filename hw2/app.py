a559d1296c2758081969ddd8a0a43963ae09c09a
Elad Goldberg <eladg@evolution.inc>
2021-06-20 21:08:35 +0300

    less strict security group

--- hw2/app.py
+++ hw2/app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 @app.route('/nodes', methods=['GET', 'POST'])
 def get_live_node_list_test():
     try:
-        target_group = elb.describe_target_groups(Names=["ShaiEladTargetGroup-tg"])
+        target_group = elb.describe_target_groups()
     except:
         return target_group
     try:
