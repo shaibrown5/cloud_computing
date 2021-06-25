47816f80cb13b45bd8aad0c7060c7ff78651d04a
Shai Brown <shaibrown95@gmail.com>
2021-06-25 23:07:32 +0300

    added hash stuff

--- hw2/app.py
+++ hw2/app.py
@@ -6,6 +6,7 @@ from flask import Flask, request
 import requests
 import boto3
 import time
+from uhashring import HashRing
 
 dynamodb = boto3.resource('dynamodb', region_name="us-east-2")
 table = dynamodb.Table('aliveNodes')
@@ -14,10 +15,35 @@ delay_period = 30 * 1000
 last = 0
 ip_address = ""
 
+
 # elb = boto3.client('elbv2', region_name='us-east-2')
 # ec2 = boto3.client('ec2', region_name='us-east-2')
+
+
+def get_live_node_list():
+    """
+    list of Ip's for the node list.
+    :return:
+    """
+    try:
+        app.logger.info('get_live_node_list')
+        now = get_millis(datetime.now())
+        response = table.scan()
+        app.logger.info(f'get_live_node_list-  response: {response}')
+        nodes = []
+        for item in response['Items']:
+            if int(item['lastAlive']) >= now - delay_period:
+                nodes.append(item['ip'])
+        return nodes
+    except Exception as e:
+        # app.logger.info(f'error in get_live_node_list {e}')
+        return "failed in the get_live_node_list"
+
+
 cache = {}
 app = Flask(__name__)
+# this will hold the nodes , and hash them consistentaly
+nodes_hash = HashRing(nodes=get_live_node_list())
 
 
 @app.route('/health-check', methods=['GET', 'POST'])
@@ -34,37 +60,27 @@ def get_millis(dt):
     return int(round(dt.timestamp() * 1000))
 
 
-def get_live_node_list():
-    try:
-        app.logger.info('get_live_node_list')
-        now = get_millis(datetime.now())
-        response = table.scan()
-        app.logger.info(f'get_live_node_list-  response: {response}')
-        nodes = []
-        for x in response['Items']:
-            if int(x['lastAlive']) >= now - delay_period:
-                nodes.append(x['ip'])
-        return nodes
-    except Exception as e:
-        # app.logger.info(f'error in get_live_node_list {e}')
-        return "failed in the get_live_node_list"
-
-
 @app.route('/put', methods=['GET', 'POST'])
 def put():
-    nodes = get_live_node_list()
+    #    nodes = get_live_node_list()
     key = request.args.get('str_key')
     data = request.args.get('data')
     expiration_date = request.args.get('expiration_date')
 
-    key_v_node_id = xxhash.xxh64_intdigest(key) % 1024
+    # key_v_node_id = xxhash.xxh64_intdigest(key) % 1024
+    #
+    # node = nodes[(key_v_node_id % len(nodes))]
+    # alt_node = nodes[((key_v_node_id + 1) % len(nodes))]
 
-    node = nodes[(key_v_node_id % len(nodes))]
-    alt_node = nodes[((key_v_node_id + 1) % len(nodes))]
+    update_live_nodes()
+    node_ip = nodes_hash.get_node(key)
+    second_node_ip = get_second_node_ip(key)
 
     try:
-        ans = requests.post(f'http://{node}:8080/set_val?str_key={key}&data={data}&expiration_date={expiration_date}')
-        ans = requests.post(f'http://{alt_node}:8080/set_val?str_key={key}&data={data}&expiration_date={expiration_date}')
+        ans = requests.post(
+            f'http://{node_ip}:8080/set_val?str_key={key}&data={data}&expiration_date={expiration_date}')
+        ans = requests.post(
+            f'http://{second_node_ip}:8080/set_val?str_key={key}&data={data}&expiration_date={expiration_date}')
     except requests.exceptions.ConnectionError:
         ans = json.dumps({'status_code': 404})
 
@@ -100,21 +116,25 @@ def set_val():
 #  get items from nodes
 @app.route('/get', methods=['GET', 'POST'])
 def get():
-    nodes = get_live_node_list()
     key = request.args.get('str_key')
 
-    key_v_node_id = xxhash.xxh64_intdigest(key) % 1024
-
-    node = nodes[key_v_node_id % len(nodes)]
-    alt_node = nodes[((key_v_node_id + 1) % len(nodes))]
+    # key_v_node_id = xxhash.xxh64_intdigest(key) % 1024
+    #     #
+    #     # node = nodes[key_v_node_id % len(nodes)]
+    #     # alt_node = nodes[((key_v_node_id + 1) % len(nodes))]
+    update_live_nodes()
+    node_ip = nodes_hash.get_node(key)
 
     try:
-        ans = requests.get(f'http://{node}:8080/get_val?str_key={key}')
-    except:
-        try:
-            ans = requests.get(f'https://{alt_node}:8080/get_val?str_key={key}')
-        except requests.exceptions.ConnectionError:
-            return ans
+        ans = requests.get(f'http://{node_ip}:8080/get_val?str_key={key}')
+    except requests.exceptions.ConnectionError:
+        ans = json.dumps({'status_code': 404})
+    # except:
+    #     try:
+    #         ans = requests.get(f'https://{alt_node}:8080/get_val?str_key={key}')
+    #     except requests.exceptions.ConnectionError:
+    #         return ans
+
     return ans.json().get('item')
 
 
@@ -128,23 +148,21 @@ def get_val():
     return response
 
 
-# @app.route('/health-check', methods=['GET', 'POST'])
-# def health_check():
-#     return "ah ah ah ah staying alive"
+def update_live_nodes():
+    nodes_list = get_live_node_list()
+
+    for node_key in nodes_hash.nodes:
+        if node_key not in nodes_list:
+            nodes_list.remove_node(node_key)
 
 
-# def signal_alive():
-#     threading.Timer(10.0, signal_alive).start()
-#     ip = socket.gethostbyname(socket.gethostname())
-#     timestamp = str(datetime.now())
-#     print(timestamp)
-#     # item = {'ip': ip,
-#     #         'last_timestamp': timestamp
-#     #         }
-#     # table.put_item(Item=item)
-#
-#
-# signal_alive()
+def get_second_node_ip(key):
+    original_node = nodes_hash.get_node(key)
+    nodes_hash.remove_node(original_node)
+    second_node = nodes_hash.get_node(key)
+    nodes_hash.add_node(original_node)
+
+    return second_node
 
 
 if __name__ == '__main__':
